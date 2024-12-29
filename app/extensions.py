bbdda38211a1674213dcc0fb31fa18a770183237
Deany256 <gdeandude@gmail.com>
2024-12-29 21:33:38 +0000

    Update extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from .yt_dlp_utils import YTDLPUtils
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+yt_utils = YTDLPUtils()
